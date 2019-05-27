001179e3d3535eba707474c6572995fcb69074b8
Jesse Gjerde <jessegjerde89@gmail.com>
2019-05-26 20:55:27 -0500

    map working again

--- src/Components/TruckMapContainer.js
+++ src/Components/TruckMapContainer.js
@@ -1,13 +1,15 @@
 import React from "react";
 import TrucksMap from "../Components/TrucksMap"
 
+
 export default class TruckMapContainer extends React.Component {
 
 
 	
 	render() {
 
-		const API_KEY = process.env.local.REACT_APP_GOOGLE_KEY;
+		const API_KEY = process.env.REACT_APP_GOOGLE_KEY;
+		
 		return (
 			<TrucksMap
 				trucks={this.props.trucks}
