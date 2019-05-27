292b9f5de83cfb4c8e5ecb8711ad94f13b4e5c6f
Jesse Gjerde <jessegjerde89@gmail.com>
2019-05-26 20:52:43 -0500

    without key

--- src/Components/TruckMapContainer.js
+++ src/Components/TruckMapContainer.js
@@ -3,11 +3,15 @@ import TrucksMap from "../Components/TrucksMap"
 
 export default class TruckMapContainer extends React.Component {
 
+
+	
 	render() {
+
+		const API_KEY = process.env.local.REACT_APP_GOOGLE_KEY;
 		return (
 			<TrucksMap
 				trucks={this.props.trucks}
-				googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyAzJ19_bp7AKuQZ9wkX6vBrSxL4P03HdG8&v=3.exp&libraries=geometry,drawing,places`}
+				googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${API_KEY}&v=3.exp&libraries=geometry,drawing,places`}
 				loadingElement={<div style={{ height: `100%` }} />}
 				containerElement={<div style={{ height: `600px`, width: `600px` }} />}
 				mapElement={<div style={{ height: `100%` }} />}
